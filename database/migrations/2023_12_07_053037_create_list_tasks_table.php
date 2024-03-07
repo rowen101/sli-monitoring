f32bf664ba709cd8800d3932adc4ad41ee490d59
Rowen <rgrowengonzales66@gmail.com>
2024-03-07 20:18:20 +0800

    update

--- database/migrations/2023_12_07_053037_create_list_tasks_table.php
+++ database/migrations/2023_12_07_053037_create_list_tasks_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tbl_tasklist', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('dailytask_id')->constrained();
+            $table->foreignId('dailytask_id')->references('dailytask_id')->on('tbl_dailytask');
             $table->string('task_name');
             $table->integer('iscompleted')->default(0)->nullable();
             $table->timestamps();
