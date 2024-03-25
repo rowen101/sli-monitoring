f6041bed0b56c723d392434e2f17838b5a27071e
Rowen <rgrowengonzales66@gmail.com>
2024-03-25 11:13:13 +0800

    update migration site user

--- database/migrations/2024_03_22_050832_create_user_sites_table.php
+++ database/migrations/2024_03_22_050832_create_user_sites_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('user_sites', function (Blueprint $table) {
-            $table->increments('id');
+         
             $table->integer('user_id');
             $table->integer('site_id');
             $table->timestamps();
